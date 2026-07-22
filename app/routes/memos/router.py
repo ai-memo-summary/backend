060af0ed60da43468c4ee6e8de85c2e51a935825
임건표 <imgeonpyo@imgeonpyoui-MacBookAir.local>
2026-07-22 21:27:58 +0900

    메모리 리스트에 저장하고 조회하는 api 추가

--- app/routes/memos/router.py
+++ app/routes/memos/router.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, status
 
 from app.schemas import MemoCreate, MemoResponse
 
+memos =[]
+next_memo_id = 1
+
 router = APIRouter(
     prefix="/memos",
     tags = ["memos"],
@@ -13,8 +16,20 @@ router = APIRouter(
     status_code=status.HTTP_201_CREATED
 )
 def create_memo(memo: MemoCreate):
-    return {
-        "id": 1,
+    global next_memo_id
+    new_memo = {
+        "id": next_memo_id,
         "title": memo.title,
         "content": memo.content
     }
+    memos.append(new_memo)
+    next_memo_id += 1
+    
+    return new_memo
+
+@router.get(
+    "",
+    response_model = list[MemoResponse],
+)
+def read_memos():
+    return memos
